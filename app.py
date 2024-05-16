cb132b98b7ea2b03b7de2d123051d6e2cb48dafa
santiagoruizm01 <160878816+santiagoruizm01@users.noreply.github.com>
2024-05-16 11:34:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
 st.title('PDF Assistant')
-st.title('Chatea con tu PDF 💬')
+st.header('Chatea con tu PDF 💬')
 ke = st.text_input('Ingresa tu Clave')
 #os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 os.environ['OPENAI_API_KEY'] = ke
