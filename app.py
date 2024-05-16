dbdcdd2bfc5540dbd5534c33b20582feefb98736
santiagoruizm01 <160878816+santiagoruizm01@users.noreply.github.com>
2024-05-16 10:59:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -61,25 +61,25 @@ if pdf is not None:
           print(cb)
         st.write(response)
 
-text = response # st.text_input("Ingrese el texto.")
+text_2 = response # st.text_input("Ingrese el texto.")
 
 tld="es"
 
 def text_to_speech(text, tld):
     
-    tts = gTTS(text,"es", tld, slow=False)
+    tts = gTTS(text_2,"es", tld, slow=False)
     try:
-        nombre_archivo = text[0:20]
+        nombre_archivo = text_2[0:20]
     except:
         nombre_archivo = "audio"
     tts.save(f"temp/{nombre_archivo}.mp3")
-    return nombre_archivo, text
+    return nombre_archivo, text_2
 
 
 #display_output_text = st.checkbox("Verifica el texto")
 
 if st.button("convertir"):
-    result, output_text = text_to_speech(text, tld)
+    result, output_text = text_to_speech(text_2, tld)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
     st.markdown(f"## Tú audio:")
