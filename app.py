5a7a555a773e24aa7c0943630a7ee08c821dfe09
santiagoruizm01 <160878816+santiagoruizm01@users.noreply.github.com>
2024-05-16 11:57:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def text_to_speech(text, tld):
 
 #display_output_text = st.checkbox("Verifica el texto")
 
-if st.button("Lee en Voz alta"):
+if st.button("Respuesta en Voz"):
     result, output_text = text_to_speech(response, tld)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
